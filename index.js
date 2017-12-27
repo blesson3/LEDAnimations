a9f13b459ebeea50a786930570ed330cd1f3cbe9
Matt Blessed <matthew.blessed@hope.edu>
2017-12-27 18:24:37 -0500

    initial commit with boilerplate code

---
+++ index.js
@@ -0,0 +1,9 @@
+const NUM_LEDS = 100;
+const GPIO_PIN = 13;
+
+let strip = require('rpi-ws281x-native');
+strip.init(NUM_LEDS, { gpio: GPIO_PIN })
+strip.reset();
+
+// Two red pixels
+strip.render([ 0xff0000, 0xff0000 ]);
