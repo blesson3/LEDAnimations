7a411f78619c952851019d6810b4e4bb3ed2797f
Matt Blessed <matthew.blessed@hope.edu>
2017-12-27 18:55:50 -0500

    added an animation loop

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const NUM_LEDS = 100;
-const GPIO_PIN = 13;
+// const GPIO_PIN = 13;
 
 let strip = require('rpi-ws281x-native');
-strip.init(NUM_LEDS, { gpio: GPIO_PIN });
+ws281x.init(NUM_LEDS);
 
-// Two red pixels
-strip.render([ 0xff0000, 0xff0000 ]);
+const pixelData = new Uint32Array(NUM_LEDS);
+
+// ---- trap the SIGINT and reset before exit
+process.on('SIGINT', function () {
+  ws281x.reset();
+  process.nextTick(function () { process.exit(0); });
+});
+
+// ---- animation-loop
+var offset = 0;
+setInterval(function () {
+  var i=NUM_LEDS;
+  while(i--) {
+      pixelData[i] = 0;
+  }
+  pixelData[offset] = 0xffffff;
+
+  offset = (offset + 1) % NUM_LEDS;
+  ws281x.render(pixelData);
+}, 100);
