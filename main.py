ed4bcaa61997f19e9bbba74d69f6d5d5de15877d
Matt Blessed <matthew.blessed@hope.edu>
2017-12-31 12:05:43 -0500

    all leds

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def opt_parse():
         return args
 
 # LED strip configuration:
-LED_COUNT      = 10      # Number of LED pixels.
+LED_COUNT      = 300      # Number of LED pixels.
 LED_PIN        = 13      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
